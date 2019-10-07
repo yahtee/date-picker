d14283d1e591919fd2717315d8109916cec60290
lazarljubenovic <lazar.ljubenovic@fazi.rs>
2019-10-07 12:43:16 +0200

    fix: type

--- src/lib/src/util/pipes.ts
+++ src/lib/src/util/pipes.ts
@@ -37,7 +37,7 @@ export class AddMonthsPipe implements PipeTransform {
 
 @Pipe({name: 'firstWeek'})
 export class FirstWeekPipe implements PipeTransform {
-  public transform(matrix: Date[][]): {$implicit: Date}[] | null {
+  public transform(matrix: (Date | null)[][]): {$implicit: Date}[] | null {
     if (matrix == null) {
       return null
     } else {
